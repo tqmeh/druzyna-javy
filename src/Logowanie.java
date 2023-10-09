75357dd0d23cbcd471a5a92fb40cc36b697dbf74
Tomek <gorniktrans@gmail.com>
2023-10-09 20:28:11 +0200

    dodawanie plików

---
+++ src/Logowanie.java
@@ -0,0 +1,53 @@
+import javax.swing.*;
+
+
+public class Logowanie extends JFrame {
+
+JLabel lLogin,lHaslo;
+JTextField tWpiszLogin;
+JPasswordField tWpiszHaslo;
+JButton bZaloguj,bRejestruj;
+    public Logowanie()
+    {
+        setSize (400,200);
+        setLayout(null);
+        setTitle("Logowanie");
+
+
+        lLogin=new JLabel("Login:");
+        lLogin.setBounds(100,20,100,20);
+        add(lLogin);
+
+        lHaslo=new JLabel("Hasło:");
+        lHaslo.setBounds(100,45,100,20);
+        add(lHaslo);
+
+        tWpiszLogin=new JTextField(" ");
+        tWpiszLogin.setBounds(150,20,100,20);
+        add(tWpiszLogin);
+
+        tWpiszHaslo=new JPasswordField();
+        tWpiszHaslo.setBounds(150,45,100,20);
+        add(tWpiszHaslo);
+
+
+
+        bZaloguj=new JButton("Zaloguj");// przycisk do logowania
+        bZaloguj.setBounds(75,100,100,20);
+        add(bZaloguj);
+       bZaloguj.addActionListener(e ->{
+           Glowna glowna=new Glowna();
+           glowna.setVisible(true);
+       });
+
+        bRejestruj=new JButton("Rejestruj");// przycisk do nowej rejestracji uzytkownika
+        bRejestruj.setBounds(175,100,100,20);
+        add(bRejestruj);
+       bRejestruj.addActionListener(e -> {
+           Rejestruj rejestruj=new Rejestruj();
+           rejestruj.setVisible(true);
+       });
+
+
+    }
+}
---
+++ src/Rejestruj.java
@@ -0,0 +1,69 @@
+import javax.swing.*;
+
+
+public class Rejestruj extends JFrame {
+
+    JLabel lLogin,lMail,lPowtorzMail,lHaslo,lPowtorzhaslo;
+    JTextField tLogin,tMail,tPowtorzMail;
+    JPasswordField pHaslo,pPowtorzHaslo;
+    JButton bRejestruj,bWyjdz;
+    public Rejestruj()
+    {
+        setSize(400,220);
+        setLayout(null);
+        setTitle("Rejestracja");
+
+        lLogin=new JLabel("Wpisz Login");
+        lLogin.setBounds(75,20,100,20);
+        add(lLogin);
+
+        tLogin=new JTextField(" ");
+        tLogin.setBounds(175,20,100,20);
+        add(tLogin);
+
+
+        lHaslo=new JLabel("Wpisz hasło");
+        lHaslo.setBounds(75,45,100,20);
+        add(lHaslo);
+
+        pHaslo=new JPasswordField();
+        pHaslo.setBounds(175,45,100,20);
+        add(pHaslo);
+
+        lPowtorzhaslo=new JLabel("Powtórz hasło");
+        lPowtorzhaslo.setBounds(75,70,100,20);
+        add(lPowtorzhaslo);
+
+        pPowtorzHaslo=new JPasswordField();
+        pPowtorzHaslo.setBounds(175,70,100,20);
+        add(pPowtorzHaslo);
+
+        lMail=new JLabel("Wpisz mail");
+        lMail.setBounds(75,95,100,20);
+        add(lMail);
+
+        tMail=new JTextField();
+        tMail.setBounds(175,95,100,20);
+        add(tMail);
+
+
+        lPowtorzMail=new JLabel("Powtórz mail");
+        lPowtorzMail.setBounds(75,120,100,20);
+        add(lPowtorzMail);
+
+        tPowtorzMail=new JTextField();
+        tPowtorzMail.setBounds(175,120,100,20);
+        add(tPowtorzMail);
+
+        bRejestruj=new JButton("Rejestruj");//przycisk rejestruj jeszcze do oprogramowania
+        bRejestruj.setBounds(100,150,100,20);
+        add(bRejestruj);
+
+        bWyjdz=new JButton("Wyjdź");//przycisk wyjdz
+        bWyjdz.setBounds(200,150,100,20);
+        add(bWyjdz);
+        bWyjdz.addActionListener(e -> dispose());
+
+
+    }
+}
