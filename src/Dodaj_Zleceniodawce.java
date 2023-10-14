b3adeb7aa8975e23d64d6ab942773e8a1fd3f9ec
Tomek <gorniktrans@gmail.com>
2023-10-14 21:21:40 +0200

    dodanie panelu dla serwisanta

--- src/Dodaj_Zleceniodawce.java
+++ src/Dodaj_Zleceniodawce.java
@@ -2,32 +2,19 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Dodaj_Zleceniodawce extends JFrame {
-JLabel lNazwaSkroconaZleceniodawcy,lZleceniodawca,lMiasto,lKodPocztowy,lUlica,lUkosnik,lNip;
-JTextField tNazwaSkroconaZleceniodawcy,tMiasto,tKodPocztowy,tUlica,tNumerDomu,tNumerMieszkania,tNip,tKraj/*usunac kraj jak zrobi sie liste*/;
+JLabel lNazwaSkroconaZleceniodawcy,lZleceniodawca,lMiasto,lKodPocztowy,lUlica,lUkosnik,lNip,lTelefonKomorkowy,lTelefonStacjonarny;
+JTextField tNazwaSkroconaZleceniodawcy,tMiasto,tKodPocztowy,tUlica,tNumerDomu,tNumerMieszkania,tNip,tKraj,tTelefonStacjonarny,tTelefonKomorkowy/*usunac kraj jak zrobi sie liste*/;
 JButton bZapisz,bWyjscie;
 JTextArea tZleceniodawca;
     JScrollPane scroll;
 JPanel panel1,panel2,panel3;
+
     public Dodaj_Zleceniodawce()
     {
         setSize(400, 400);
         setLayout(new BorderLayout());
         setExtendedState(MAXIMIZED_BOTH);
-        lNazwaSkroconaZleceniodawcy=new JLabel();
-        lZleceniodawca=new JLabel();
-        lMiasto=new JLabel();
-        lKodPocztowy=new JLabel();
-        lUlica=new JLabel();
-        lUkosnik=new JLabel();
-        lNip=new JLabel();
-        tNazwaSkroconaZleceniodawcy=new JTextField();
-        tMiasto=new JTextField();
-        tKodPocztowy=new JTextField();
-        tUlica=new JTextField();
-        tNumerDomu=new JTextField();
-        tNumerMieszkania=new JTextField();
-        tNip=new JTextField();
-        tKraj=new JTextField();
+
         panel1=stworzPanel1();
         panel2=stworzPanel2();
         panel3=stworzPanel3();
@@ -51,6 +38,8 @@ private JPanel stworzPanel1()
         String NumerDomu=tNumerDomu.getText().trim();
         String NumerMieszkania=tNumerMieszkania.getText().trim();
         String NIP=tNip.getText().trim();
+        String TelefonStacjonarny=tTelefonStacjonarny.getText().trim();
+        String TelefonKomorkowy=tTelefonKomorkowy.getText().trim();
 
         System.out.println(NazwaSkroconaZleceniodawcy);// do wyrzucenia po sprawdzeniu
         System.out.println(PelnaNazwaZleceniodawcy);
@@ -85,27 +74,38 @@ private JPanel stworzPanel1()
         {
             WyswietlKomunikatoBledzie("Nie wpisales numeru domu");
         }
+        else if (TelefonStacjonarny.isEmpty())
+        {
+            if(TelefonKomorkowy.isEmpty())
+            {
+                WyswietlKomunikatoBledzie("Nie wpisaleś numeru telefonu komórkowego bądź stacjoanrnego");
+            }
+        }
 
-        if(!NazwaSkroconaZleceniodawcy.isEmpty()&&!PelnaNazwaZleceniodawcy.isEmpty()&&!Miasto.isEmpty()&&!KodPocztowy.isEmpty()&&!Ulica.isEmpty()&&!NumerDomu.isEmpty())
+
+    /* if(!NazwaSkroconaZleceniodawcy.isEmpty()||!PelnaNazwaZleceniodawcy.isEmpty()||!Miasto.isEmpty()||!KodPocztowy.isEmpty()||!Ulica.isEmpty()||!NumerDomu.isEmpty())
         {
             if(NIP.isEmpty())
             {
-
-                int odpowiedz=JOptionPane.showConfirmDialog(null,"Czy zapisac zleceniodawce bez Nipu?","Potwierdzenie",JOptionPane.YES_NO_OPTION);
+                JOptionPane nowy=new JOptionPane();
+                int odpowiedz=nowy.showConfirmDialog(null,"Czy zapisac zleceniodawce bez Nipu?","Potwierdzenie",JOptionPane.YES_NO_OPTION);
                 if(odpowiedz==JOptionPane.YES_OPTION)
                 {
-                    System.out.println("zapisuje bez nipu");
+                    System.out.println("zapisuje bez NIPu");
                     dispose();
                 }
                 else
                 {
-                    JOptionPane.getRootFrame().dispose();
-
+                  Window w=SwingUtilities.getWindowAncestor(nowy);
+                   if(w !=null)
+                   {
+                       w.setVisible(false);
+                   }
                 }
             }
             System.out.println("Zapisuje zleceniodawce");
             dispose();
-        }
+        } do porpawy jeden warning*/
     });
 
     bWyjscie=new JButton("Wyjscie"); // przycisk bWysjcie
@@ -133,6 +133,26 @@ private JPanel stworzPanel3()
         add(panel, BorderLayout.CENTER);
         panel.setLayout(null);
 
+        lNazwaSkroconaZleceniodawcy=new JLabel();
+        lZleceniodawca=new JLabel();
+        lMiasto=new JLabel();
+        lKodPocztowy=new JLabel();
+        lUlica=new JLabel();
+        lUkosnik=new JLabel();
+        lNip=new JLabel();
+        lTelefonStacjonarny=new JLabel();
+        tNazwaSkroconaZleceniodawcy=new JTextField();
+        tMiasto=new JTextField();
+        tKodPocztowy=new JTextField();
+        tUlica=new JTextField();
+        tNumerDomu=new JTextField();
+        tNumerMieszkania=new JTextField();
+        tTelefonStacjonarny=new JTextField();
+        lTelefonKomorkowy=new JLabel();
+        tNip=new JTextField();
+        tKraj=new JTextField();
+        tTelefonKomorkowy=new JTextField();
+
         WysweitlNapis(lNazwaSkroconaZleceniodawcy,"Nazwa skrócona zleceniodawcy",20,0,200,20,panel);
         WpiszTekst(tNazwaSkroconaZleceniodawcy,20,20,200,20,panel);
         WysweitlNapis(lZleceniodawca,"Pelna nazwa zleceniodawcy",20,50,200,20,panel);
@@ -156,7 +176,12 @@ private JPanel stworzPanel3()
         WpiszTekst(tNumerMieszkania,210,340,20,20,panel);
         WysweitlNapis(lNip,"NIP",400,20,40,20,panel);
         WpiszTekst(tKraj,430,20,20,20,panel);
-        WpiszTekst(tNip,435,20,100,20,panel);
+        WpiszTekst(tNip,455,20,100,20,panel);
+        WysweitlNapis(lTelefonStacjonarny,"Telefon stacjonarny",400,50,150,20,panel);
+        WpiszTekst(tTelefonStacjonarny,400,80,150,20,panel);
+        WysweitlNapis(lTelefonKomorkowy,"Telefon komórkowy",400,110,150,20,panel);
+        WpiszTekst(tTelefonKomorkowy,400,140,150,20,panel);
+
 
 
         return panel;
